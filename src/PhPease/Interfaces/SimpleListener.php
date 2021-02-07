b93025758fbcf103233fcee22eb73cbef665b0bb
ifeelweb <info@ifeelweb.de>
2021-02-07 12:30:37 +0100

    added interface SimpleListener

---
+++ src/PhPease/Interfaces/SimpleListener.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the PhPease package.
+ *
+ * (c) Timo Reith <mail@timoreith.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PhPease\Interfaces;
+
+interface SimpleListener
+{
+    public function startListening();
+
+    public function stopListening();
+}
