8a97fc376ac74a91b0ad4ea62d83cd5de9d7312a
info@ifeelweb.de <%5uper0gFC%>
2023-12-26 13:31:28 +0100

    added Conf functions

--- tests/ConfTest.php
+++ tests/ConfTest.php
@@ -5,7 +5,7 @@ use function PhPease\Convert\kilobytes_to_human_readable;
 
 final class ConfTest extends TestCase
 {
-    public function test_get_memory_limit()
+    public function test_get_memory_limit_128M()
     {
         ini_set('memory_limit', '128M');
 
@@ -13,5 +13,15 @@ final class ConfTest extends TestCase
 
         $this->assertEquals(131072, $memLimit);
         $this->assertEquals('128.00 MB', kilobytes_to_human_readable($memLimit));
+    }
+
+    public function test_get_memory_limit_64M()
+    {
+        ini_set('memory_limit', '64M');
+
+        $memLimit = get_memory_limit();
+
+        $this->assertEquals(65536, $memLimit);
+        $this->assertEquals('64.00 MB', kilobytes_to_human_readable($memLimit));
     }
 }
